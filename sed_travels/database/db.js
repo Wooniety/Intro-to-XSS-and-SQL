db143b36af0cca3818659aeb0096b9bef5fbfadd
Wooniety <wooniety@gmail.com>
2020-12-27 16:14:32 +0800

    smol changes

--- sed_travels/database/db.js
+++ sed_travels/database/db.js
@@ -1,25 +1,5 @@
-/*
-Class: DISM/FT/2A/02
-Group:
-Members:
-1. Loh Kar Wei 1904204
-2. Jess Kwek 1928934
-*/
-
 const mysql = require("mysql2");
 const sqlite = require("sqlite3").verbose();
-// const dbconnect = {
-//     getConnection: ()=>{
-//         const conn = mysql.createPool({
-//             host:"localhost",
-//             port: 3306,
-//             user: "pikachu",
-//             password: "password",
-//             database: "SPTRAVEL"
-//         }).promise();
-//         return conn;
-//     }
-// }
 const dbconnect = {
     getConnection: ()=>{
         const conn = new sqlite.Database("./sad.sqlite",(err)=>{
--- sed_travels/model/user.js
+++ sed_travels/model/user.js
@@ -53,7 +53,7 @@ const login_user = async (email, password) => {
     const sql = "select email from users where email = '" + email + "'";
     const salt = await dbConn.query(sql,[]);
     const given_hash = await generate_hash_password(password, salt.rows[0].salt);
-    const results = await dbConn.query("select password, salt, userid, role, username from users where email = '" + email + "' and password = '" + given_hash + "'",[]);
+    const results = await dbConn.query("select password, salt, userid, role, username from users where email = '" + email + "' and password = '" + given_hash[0] + "'",[]);
     if (results.length == 0) throw new Error("Wrong password");
     else {
         return {"message":`Login Successful, welcome user ${results.rows[0].username}`,"user":{"userid":results.rows[0].userid,"role":results.rows[0].role}};
